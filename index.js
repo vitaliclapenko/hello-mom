dc5cf58930c28744b014a3b1408244a3bc4b1992
Vitalname <prostovitalic@gmail.com>
2024-12-22 13:36:09 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+let button = document.querySelector("button");
+let output = document.querySelector(".output");
+
+button.addEventListener("click", function(){
+    output.innerHTML = "Mom, I love you!";
+});
